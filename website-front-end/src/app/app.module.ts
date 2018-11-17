fd7b5c7307505bfe599e13cd0ff8bc37739a4dd1
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2018-11-17 16:27:03 +0000

    Basic angular stuff

--- website-front-end/src/app/app.module.ts
+++ website-front-end/src/app/app.module.ts
@@ -4,11 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { AngularComponent } from './component/angular/angular.component';
+import { HomeComponent } from './component/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'angular', component: AngularComponent, pathMatch: 'full' }
+];
+
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
